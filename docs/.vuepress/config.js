4b1cc1bf63b91f2526e1929dcc0fe033a09e5475
rongkz <rkz123456>
2020-12-14 15:51:38 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,26 @@
 module.exports = {
-    title: "鸿鹄笔记",
-    description: "鸿鹄的个人笔记",
+    base: '/keith-book/',
+    title: '鸿鹄笔记',
+    description: '鸿鹄的个人笔记',
     head: [
         ['meta', { rel: 'author', content: 'Keith' }],
         ['link', { rel: 'icon', content: '/favicon.ico' }]
     ],
     themeConfig: {
         lastUpdated: 'Last Updated',
-        logo: '/assets/img/logo.png',
+        logo: '/favicon.ico',
         nav: [
             { text: 'Home', link: '/' },
             { text: 'Linux', link: '/linux/' },
-            { text: 'About', link: '/about/' }
+            { text: 'Java', link: '/java/' }
         ],
         sidebar: {
             '/linux/': [
                 {
-                    title: 'Docker',   // 必要的
-                    path: '/linux/docker/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                    collapsable: false, // 可选的, 默认值是 true,
-                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    title: 'Docker',
+                    path: '/linux/docker/',
+                    collapsable: false,
+                    sidebarDepth: 1,
                     children: [
                         '/linux/docker/Docker基本操作',
                         '/linux/docker/Docker安装Redis',
@@ -29,38 +30,32 @@ module.exports = {
                     ]
                 },
                 {
-                    title: 'Test',   // 必要的
-                    path: '/linux/test/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                    collapsable: false, // 可选的, 默认值是 true,
-                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    title: 'Test',
+                    path: '/linux/test/',
+                    collapsable: false,
+                    sidebarDepth: 1,
                     children: [
                         '/linux/test/test1',
                         '/linux/test/test2',
                     ]
                 }
             ],
-            '/about/': [
+            '/java/': [
                 {
-                    title: 'About',   // 必要的
-                    path: '/about/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                    collapsable: false, // 可选的, 默认值是 true,
-                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    title: 'Java',
+                    path: '/java/',
+                    collapsable: false,
+                    sidebarDepth: 1,
                     children: [
-                        '/about/aboutTest',
-                        '/about/aboutTest2',
+                        '/java/java基础/反射',
+                        '/java/java基础/集合',
+                        '/java/java基础/锁',
+                        '/java/java基础/线程池',
+                        '/java/java基础/IO流',
+                        '/java/java基础/JVM',
                     ]
                 }
             ]
         }
-
-        // '/linux/': [
-        //     '/linux/docker/Docker基本操作',
-        //     '/linux/docker/Docker安装Redis',
-        //     '/linux/docker/Docker安装Nginx',
-        //     '/linux/docker/Docker安装Mysql',
-        //     '/linux/docker/Docker安装Jenkins',
-        //     '/linux/test/test1',
-        //     '/linux/test/test2',
-        // ],
     }
 }
